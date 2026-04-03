70a7112973b0509cc85114db8b4a83156e75e779
XercoDylan <xerco@mit.edu>
2026-04-03 13:55:42 -0400

    fixed routing

--- api/index.py
+++ api/index.py
@@ -1,33 +1,14 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
-origins = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-app.mount("/Assets", StaticFiles(directory="Assets"), name="assets")
-app.mount("/story", StaticFiles(directory="Story"), name="story")
-app.mount("/home", StaticFiles(directory="Home"), name="home")
 
 
 @app.get("/")
 async def read_index():
-    return RedirectResponse(url="/home/home.html")
+    return RedirectResponse(url="/Home/home.html")
 
 
 @app.get("/story")
-async def read_index():
-    return RedirectResponse(url="/story/story.html")
+async def read_story():
+    return RedirectResponse(url="/Story/story.html")
